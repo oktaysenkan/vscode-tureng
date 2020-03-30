51602682e002e294d406dccbd45b44a597d339d8
Oktay Şenkan <oktay.senkan@gmail.com>
2020-03-30 04:48:37 +0300

    input text and suggestion list are added.

--- src/extension.ts
+++ src/extension.ts
@@ -2,10 +2,9 @@ import * as vscode from "vscode";
 import translate from "./translate";
 
 export const activate = (context: vscode.ExtensionContext) => {
-  let disposable = vscode.commands.registerCommand(
-    "extension.tureng",
-    translate
-  );
+  let disposable = vscode.commands.registerCommand("extension.tureng", () => {
+    translate();
+  });
 
   context.subscriptions.push(disposable);
 };
--- src/translate.ts
+++ src/translate.ts
@@ -5,40 +5,58 @@ const editor = vscode.window.activeTextEditor;
 
 import config from "./configs";
 
-const translate = () => {
-  const selectedText = editor!.document.getText(editor!.selection);
+const translate = async (text?: string) => {
+  const selectedText = text || editor?.document.getText(editor?.selection);
 
   if (!selectedText) {
-    vscode.window.showErrorMessage(config.TEXT_NOT_SELECTED);
+    const inputText = await vscode.window.showInputBox({
+      placeHolder: "Turkish or English"
+    });
+
+    if (inputText) {
+      translate(inputText);
+    }
+
     return;
   }
 
   const word = new Tureng(selectedText, config.TRANSLATION_LANGUAGES);
 
-  word.Translate((list: any) => {
+  word.Translate(async (list: any) => {
     if (!list.Situation.IsFound) {
       if (list.Suggestions.length < 1) {
         vscode.window.showErrorMessage(config.TRANSLATION_NOT_FOUND);
         return;
       }
 
-      const suggestions = list.Suggestions.join(", ");
-      vscode.window.showWarningMessage(`Suggestions: ${suggestions}`);
+      const suggestions: [] = list.Suggestions;
+      const suggestionsButtons = suggestions.splice(0, 3);
+      const document = suggestions.join(", ");
+
+      const selection = await vscode.window.showWarningMessage(
+        `Suggestions: ${document}`,
+        ...suggestionsButtons
+      );
+
+      if (selection) {
+        translate(selection);
+      }
+
       return;
     }
 
     const results: [] = list.Translations.map((item: any) => {
-      const language: string = list.IsEn2Tr ? item.TermTR : item.TermENG;
+      const sentence: string = list.IsEn2Tr ? item.TermTR : item.TermENG;
 
-      const squareBracketIndex = language.indexOf("[");
-      const parenthesIndex = language.indexOf("(");
+      const squareBracketIndex = sentence.indexOf("[");
+      const parenthesIndex = sentence.indexOf("(");
 
       const separatorIndex =
         squareBracketIndex === -1 ? parenthesIndex : squareBracketIndex;
 
-      const rawWord = language.substring(0, separatorIndex - 1);
+      const word = sentence.substring(0, separatorIndex - 1);
 
-      return rawWord;
+      return word;
     });
 
     const document = results.join(", ");
